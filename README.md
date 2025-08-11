3170864424150a68f1e7cf09ec1c2a0161ed0f90
Ionut Cicio <ionutcicio.dev@gmail.com>
2025-08-11 11:10:08 +0200

    feat(cluster): setup job submission with Cypher

--- README.md
+++ README.md
@@ -2,23 +2,57 @@
 
 [ConventionalCommits](https://www.conventionalcommits.org/en/v1.0.0/#summary)
 
-## libroadrunner
-
-```bash
-python3 -m venv .venv
-pip install libroadrunner
-pip install neo4j
-pip install python-libsbml
-pip install PyQt5
-pip install apricopt
-pip install PyNomadBBO
-pip install petab
-```
-
 ## experiment
 
 https://reactome.org/content/detail/R-HSA-158164
 
-TODO:
-- [ ] inferredTo relation
-- [ ] model simple SBML file
+- [x] What indexes are available
+    - docs/indexes.csv
+
+- [x] MATCH?
+    - [ ] longestPath from end-node to one of its ancestors (see shortestPath) ?
+- [x] patterns
+    - https://neo4j.com/docs/cypher-manual/4.4/syntax/patterns/
+    - basically all the ones I know, literally nothing new (except the knowledge about <-\[*\]-)
+
+- [x] MERGE?
+    - basically just MATCH + CREATE (if it exists, take it, otherwise create it)
+- [x] WITH?
+    - basically allows to do a query in multiple steps, allowing multiple matches to simplify the queries
+    - it allows to bring nodes to the next MATCH in the query (MATCH 1 gets m, MATCH 2 uses m to generate other stuff)
+
+- [x] UNWIND?
+    - The UNWIND clause makes it possible to transform any list back into individual rows. These lists can be parameters that were passed in, previously collect-ed result, or other list expressions.
+
+
+- [x] test PROFILE baby!
+
+- [ ] Cluster
+    - [x] Make it work
+    - [x] Automate slurm + singularity queries with script (bash take argument for query)
+    - [x] Pass repository to cluster with SCP
+    - [ ] Find a way to pass neo4j.conf (password problem?) and check if it makes a difference
+
+- [ ] Performance 
+    - [ ] Create an index
+    - [ ] Find a way to pass it to the cluster (save a new dump + send that dump)
+    - [ ] Test custom docker image with Reactome DB + neo4j 5 (enterprise) 
+    - [ ] Test parallel query
+
+
+<!-- I could use this to generate just the physical entitites and reactions, and then work on them in the next query? -->
+
+- [ ] Planner + USING (https://neo4j.com/docs/cypher-manual/4.4/query-tuning/using/)
+
+- [x] input|output are inversed
+
+
+- [ ] What happens at depth 12?
+- [ ] How to create and index on a relation
+- [ ] EXTRA
+    - [x] hasEvent? Cosa ci facciamo?
+        - lo possiamo ignorare momentanemanet
+    - [ ] hasEncapsulatedEvent?
+- [ ] debug could help?
+
+- [ ] TODO: what if I want an entire Pathway, how should I behave?
--- reactome.sh
+++ reactome.sh
@@ -1,47 +1,63 @@
 #!/bin/bash
 
-run_docker=false # run locally with Docker
-run_docker_5=false # run locally with Docker using neo4j 5
-run_singularity=false # run on HPC cluster with slurm + singularity
-run_test=false # run a demanding test query against the database
+# Build reactome.sif image to run container.
+build_singularity_image=false 
 
-while getopts dst option; do
+# Run on HPC cluster with slurm + singularity. 
+# Requires reactome.sif image to be built.
+run_slurm_singularity_image=false 
+
+# Run reactome container locally with Docker.
+run_local_docker_image=false 
+
+# Run a test query using curl.
+run_curl_query=false 
+
+# Run a test query using cypher-shell within a running container.
+run_cypher_query=false
+
+# Run query with a srun job and save results. 
+submit_job=false
+
+# File containing the cypher query.
+cypher_file=''
+
+while getopts bcdstj: option; do
     case $option in
-        d) run_docker=true;;
-        s) run_singularity=true;;
-        t) run_test=true;;
+        b) build_singularity_image=true;;
+        s) run_slurm_singularity_image=true;;
+
+        d) run_local_docker_image=true;;
+
+        # TODO: cypher_file for curl_query too, it has to be optional;
+        t) run_curl_query=true;;
+
+        # TODO: cypher_file for cypher_query too, it has to be optional;
+        c) run_cypher_query=true;;
+
+        j) submit_job=true
+            cypher_file="$OPTARG";;
     esac
 done
 
-if $run_docker; then
+if $build_singularity_image; then
+    singularity build --sandbox reactome.sif docker://public.ecr.aws/reactome/graphdb:latest
+fi
+
+if $run_slurm_singularity_image; then
+    srun --pty singularity shell --userns --writable reactome.sif 
+fi
+
+if $run_local_docker_image; then
     docker run -p 7474:7474 -p 7687:7687 -e NEO4J_dbms_memory_heap_maxSize=4g public.ecr.aws/reactome/graphdb:latest
 fi
 
-if $run_docker_5; then
-    docker run --interactive --tty --rm \
-        --volume=$HOME/neo4j/newdata:/data \
-        --volume=$HOME/neo4j/backups:/backups \
-        neo4j/neo4j-admin:2025.06.2 \
-        neo4j-admin database load neo4j --from-path=/backups
-
-    docker run --interactive --tty --rm \
-        --volume=$HOME/neo4j/newdata:/data \
-        neo4j:2025.06.2
-fi
-
-
-if $run_singularity; then
-    srun --pty singularity shell -u --overlay overlay.img docker://public.ecr.aws/reactome/graphdb:latest
-fi
 
 NEO4J_URL_REACTOME='http://localhost:7474/db/data/transaction/commit'
 AUTH='-u neo4j:neo4j'
 
-if $run_test; then 
+if $run_curl_query; then 
     QUERY='MATCH path = (n {dbId: 158754})<-[*..12]-(r) RETURN DISTINCT r'
-    # QUERY='MATCH path = (n {dbId: 158754})<-[*..12]-(r:PhysicalEntity) WHERE NONE( x IN relationships(path) WHERE type(x) IN [\"author\", \"modified\", \"edited\", \"authored\", \"reviewed\", \"created\", \"updatedInstance\", \"revised\"]) RETURN DISTINCT r'
-    # QUERY='MATCH path = (n:PhysicalEntity {dbId: 158754})<-[*..17]-(r) WHERE ALL( x IN relationships(path) WHERE type(x) IN [\"input\", \"output\"]) RETURN DISTINCT r'
-    # QUERY='MATCH (n:PhysicalEntity {dbId: 158754})<-[*..17]-(r:ReactionLikeEvent) RETURN DISTINCT r'
 
 NEO4J_REQUEST="
 {
@@ -57,3 +73,26 @@ NEO4J_REQUEST="
     curl -X POST $NEO4J_URL_REACTOME $AUTH -H 'Content-Type: application/json' -d "$NEO4J_REQUEST" | jq '.results.[0].data' 
 fi
 
+query() {
+    cypher-shell -u neo4j -p neo4j --format verbose "$1" | tail -n 1
+}
+
+if $run_cypher_query; then 
+    query "MATCH path = (n {dbId: 158754})<-[*..12]-(r:PhysicalEntity) WHERE NONE(x IN relationships(path) WHERE type(x) IN ['author', 'modified', 'edited', 'authored', 'reviewed', 'created', 'updatedInstance', 'revised']) RETURN DISTINCT r"
+fi
+
+if $submit_job; then
+    # basically
+    # 1. run container 
+    # 2. start a script within the container, in which
+    # 3. create the log file to solve permission problem 
+    # 4. 'neo4j start' in background
+    # 5. while loop that waits until 'neo4j status' returns the port / valid info
+    # 6. run the query
+    # 7. save the results in a file (matching the query file name or stuff like that, even just giving the name as an argument, and give it a default value)
+    # 8. everyone happy!!
+fi
+
+# QUERY='MATCH path = (n {dbId: 158754})<-[*..12]-(r:PhysicalEntity) WHERE NONE( x IN relationships(path) WHERE type(x) IN [\"author\", \"modified\", \"edited\", \"authored\", \"reviewed\", \"created\", \"updatedInstance\", \"revised\"]) RETURN DISTINCT r'
+# QUERY='MATCH path = (n:PhysicalEntity {dbId: 158754})<-[*..17]-(r) WHERE ALL( x IN relationships(path) WHERE type(x) IN [\"input\", \"output\"]) RETURN DISTINCT r'
+# QUERY='MATCH (n:PhysicalEntity {dbId: 158754})<-[*..17]-(r:ReactionLikeEvent) RETURN DISTINCT r'

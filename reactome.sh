825cf46b157ff6191dc5868ef675fad60ba9d051
Ionut Cicio <ionutcicio.dev@gmail.com>
2025-08-25 22:48:14 +0200

    feat: add neo4j.conf to submission, batch jobs, fixed flags bug, change default log file

--- reactome.sh
+++ reactome.sh
@@ -13,19 +13,22 @@ run_reactome_locally_with_docker=false
 # Run a query using cypher-shell within a running container
 exec_cypher_query=false
 
-# Run query with a srun job and save results
+# Run query with a srun job and save results (requires cypher file)
 submit_job=false
 
-# File containing the cypher query
+# Run query with a sbatch job and save results (requries cypher file)
+submit_batch_job=false
+
+# File containing the cypher query (requires cypher file)
 cypher_file=''
 
-# File in which to store a job logs
-log_file='job.log'
+# File in which to store a job logs (requires log file)
+log_file=''
 
-# File in which to store the results of a query
+# File in which to store the results of a query (requires out file)
 out_file=''
 
-while getopts bc:dl:o:qsj: option; do
+while getopts bc:dl:o:q:sj: option; do
     case $option in
         b) build_reactome_sif=true;;
         s) run_reactome_sif_with_slurm_interactive=true;;
@@ -33,6 +36,7 @@ while getopts bc:dl:o:qsj: option; do
 
         c) exec_cypher_query=true; cypher_file="$OPTARG";;
         j) submit_job=true; cypher_file="$OPTARG";;
+        q) submit_batch_job=true; cypher_file="$OPTARG";;
 
         l) log_file="$OPTARG";;
         o) out_file="$OPTARG";;
@@ -61,12 +65,16 @@ exec_query_file() {
 }
 
 if $exec_cypher_query; then 
+    basename="$(basename $cypher_file '.cypher')"
+
+    if [ -z "$log_file" ]; then 
+        log_file="./logs/$basename.log"
+    fi
+
     {
         time {
             touch /var/lib/neo4j/logs/neo4j.log
-
-            export HEAP_SIZE=128g
-            # export NEO4J_CONF='./neo4j.conf'
+            cp ./neo4j.conf /var/lib/neo4j/conf/neo4j.conf
 
             neo4j start
             neo4j status
@@ -74,33 +82,53 @@ if $exec_cypher_query; then
             iteration=1
             while ! (echo > /dev/tcp/localhost/7687) 2>/dev/null; do 
                 sleep 1; 
-                echo "... $iteration"
+                echo "[$(printf "%3d" $iteration)]"
                 iteration=$((iteration + 1))
             done
         }
     } 2> $log_file 
 
-    {
+    { 
         if [ -z "$out_file" ]; then 
-            out_file="$cypher_file.out"
+            out_file="./logs/$basename.out"
         fi
 
-        time exec_query_file $cypher_file > $out_file
+        time exec_query_file $cypher_file > $out_file 
     } 2>> $log_file 
 fi
 
 if $submit_job; then
-    srun singularity run --writable reactome.sif ./reactome.sh -c $cypher_file -o $out_file -l $log_file
+    args=( "-c $cypher_file" )
+    
+    if [ -n "$out_file" ]; then 
+        args+=( "-o $out_file" ) 
+    fi
+
+    if [ -n "$log_file" ]; then 
+        args+=( "-l $log_file" ) 
+    fi
+
+    basename="$(basename $cypher_file '.cypher')"
+    srun -J $basename singularity run --writable reactome.sif ./reactome.sh "${args[@]}"
 fi
 
-# TODO: job name based on query
-# TODO: --additional-config??? Maybe not needed
-# TODO: possibly log execution time, maybe use 'tee'
-# neo4j-admin 
-# NEO4J_CONF    Path to directory which contains neo4j.conf.
-# NEO4J_DEBUG   Set to anything to enable debug output.
-# NEO4J_HOME    Neo4j home directory.
-# HEAP_SIZE     Set JVM maximum heap size during command execution. Takes a number and a unit, for example 512m.
-# 31G suggested
-# let's try 128G
+if $submit_batch_job; then
+    args=( "-c $cypher_file" )
+    
+    if [ -n "$out_file" ]; then 
+        args+=( "-o $out_file" ) 
+    fi
 
+    if [ -n "$log_file" ]; then 
+        args+=( "-l $log_file" ) 
+    fi
+
+    echo $cypher_file
+    basename="$(basename $cypher_file '.cypher')"
+    echo "#!/bin/bash" > temp.job
+    echo "singularity run --writable reactome.sif ./reactome.sh ${args[@]}" >> temp.job
+    chmod +x temp.job
+    sbatch -J $basename temp.job 
+fi
+
+# TODO: print cypher query results in JSON

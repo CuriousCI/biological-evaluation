a8c0e80935cc7901da862c675e0e0bbbba2974e7
Ionut Cicio <ionutcicio.dev@gmail.com>
2025-11-07 13:14:42 +0100

    fix: uniroma1 submit command

--- packages/buckpass/src/buckpass/submitter/uniroma1_submitter.py
+++ packages/buckpass/src/buckpass/submitter/uniroma1_submitter.py
@@ -14,18 +14,29 @@ class Uniroma1Submitter(Submitter[SlurmJobId, OpenBoxTaskId]):
 
     @override
     def submit(self, args: OpenBoxTaskId) -> SlurmJobId:
-        # ssh -i ~/.ssh/Uniroma1Cluster cicio_2048752@151.100.174.45 "echo 'hello'
         stdout: str = subprocess.run(
             [
                 "ssh",
                 "-i",
                 "~/.ssh/Uniroma1Cluster",
-                f"{os.getenv('CLUSTER_USER')}{os.getenv('FRONTEND_ADDRESS')}",
-                f'"ssh submitter \\"sbatch {os.getenv("CLUSTER_PROJECT_PATH")}/src/job.sh {args}\\""',
+                f"{os.getenv('CLUSTER_USER')}@{os.getenv('FRONTEND_ADDRESS')}",
+                f'"ssh submitter \\"sbatch {os.getenv("CLUSTER_PROJECT_PATH")}src/job.sh {args}\\""',
             ],
             check=False,
             capture_output=True,
         ).stdout.decode()
+
+        # stdout: str = subprocess.run(
+        #     [
+        #         "ssh",
+        #         "-i",
+        #         "~/.ssh/Uniroma1Cluster",
+        #         f"{os.getenv('CLUSTER_USER')}{os.getenv('FRONTEND_ADDRESS')}",
+        #         f'"ssh submitter \\"sbatch {os.getenv("CLUSTER_PROJECT_PATH")}/src/job.sh {args}\\""',
+        #     ],
+        #     check=False,
+        #     capture_output=True,
+        # ).stdout.decode()
 
         # `sbatch` prints "Submitted batch job 781422" to stdout
         return "".join(filter(str.isnumeric, stdout))

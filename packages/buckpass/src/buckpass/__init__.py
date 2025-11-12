f3126ebf5acf3ea33fa55523be85f58d0d257f8e
Ionut Cicio <ionutcicio.dev@gmail.com>
2025-11-12 17:17:51 +0100

    fix: missing exported packages

--- packages/buckpass/src/buckpass/__init__.py
+++ packages/buckpass/src/buckpass/__init__.py
@@ -1,25 +1,24 @@
 """Library to buck-pass optimization tasks to workers on a HPC cluster."""
 
-from buckpass.core import openbox_api, util
+from buckpass.core import openbox_api
 
-from .core import IntGEZ, Policy, Submitter
-from .policy.fixed_batch_policy import FixedBatchPolicy
-from .submitter.sbatch_submitter import SbatchSubmitter
-from .submitter.ssh_submitter import SshSubmitter
-from .submitter.uniroma1_submitter import Uniroma1Submitter
+from .core import IntGEZ
+from .core.policy import Policy
+from .core.submitter import Submitter
+from .policy.batch import BatchPolicy
+from .submitter.emulation import EmulationSubmitter
+from .submitter.uniroma1 import Uniroma1Submitter
 
 __version__ = version = "0.1.0"
 
 __all__ = [
-    "FixedBatchPolicy",
+    "BatchPolicy",
+    "EmulationSubmitter",
     "IntGEZ",
     "Policy",
-    "SbatchSubmitter",
-    "SshSubmitter",
     "Submitter",
     "Uniroma1Submitter",
     "__version__",
     "openbox_api",
-    "util",
     "version",
 ]
